6ee5bb23604beabbfacb1bdaadd27438097da1ae
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-08-18 13:46:03 -0300

    Replace super() method calls with self calls (#22)

--- google-datacatalog-looker-connector/src/google/datacatalog_connectors/looker/prepare/datacatalog_tag_factory.py
+++ google-datacatalog-looker-connector/src/google/datacatalog_connectors/looker/prepare/datacatalog_tag_factory.py
@@ -33,24 +33,23 @@ class DataCatalogTagFactory(prepare.BaseTagFactory):
 
         tag.template = tag_template.name
 
-        super()._set_string_field(tag, 'id', dashboard.id)
-        super()._set_string_field(tag, 'description', dashboard.description)
-        super()._set_string_field(tag, 'folder_id', dashboard.space.id)
-        super()._set_string_field(tag, 'folder_name', dashboard.space.name)
-        super()._set_bool_field(tag, 'is_hidden', dashboard.hidden)
-        super()._set_double_field(tag, 'user_id', dashboard.user_id)
-        super()._set_double_field(tag, 'view_count', dashboard.view_count)
-        super()._set_double_field(tag, 'favorite_count',
-                                  dashboard.favorite_count)
-        super()._set_timestamp_field(tag, 'last_accessed_at',
-                                     dashboard.last_accessed_at)
-        super()._set_timestamp_field(tag, 'last_viewed_at',
-                                     dashboard.last_viewed_at)
-        super()._set_bool_field(tag, 'is_deleted', dashboard.deleted)
-        super()._set_timestamp_field(tag, 'deleted_at', dashboard.deleted_at)
-        super()._set_double_field(tag, 'deleter_id', dashboard.deleter_id)
+        self._set_string_field(tag, 'id', dashboard.id)
+        self._set_string_field(tag, 'description', dashboard.description)
+        self._set_string_field(tag, 'folder_id', dashboard.space.id)
+        self._set_string_field(tag, 'folder_name', dashboard.space.name)
+        self._set_bool_field(tag, 'is_hidden', dashboard.hidden)
+        self._set_double_field(tag, 'user_id', dashboard.user_id)
+        self._set_double_field(tag, 'view_count', dashboard.view_count)
+        self._set_double_field(tag, 'favorite_count', dashboard.favorite_count)
+        self._set_timestamp_field(tag, 'last_accessed_at',
+                                  dashboard.last_accessed_at)
+        self._set_timestamp_field(tag, 'last_viewed_at',
+                                  dashboard.last_viewed_at)
+        self._set_bool_field(tag, 'is_deleted', dashboard.deleted)
+        self._set_timestamp_field(tag, 'deleted_at', dashboard.deleted_at)
+        self._set_double_field(tag, 'deleter_id', dashboard.deleter_id)
 
-        super()._set_string_field(tag, 'instance_url', self.__instance_url)
+        self._set_string_field(tag, 'instance_url', self.__instance_url)
 
         return tag
 
@@ -59,28 +58,27 @@ class DataCatalogTagFactory(prepare.BaseTagFactory):
 
         tag.template = tag_template.name
 
-        super()._set_string_field(tag, 'id', element.id)
-        super()._set_string_field(tag, 'type', element.type)
-        super()._set_string_field(tag, 'dashboard_id', element.dashboard_id)
+        self._set_string_field(tag, 'id', element.id)
+        self._set_string_field(tag, 'type', element.type)
+        self._set_string_field(tag, 'dashboard_id', element.dashboard_id)
 
         if dashboard:
-            super()._set_string_field(tag, 'dashboard_title', dashboard.title)
+            self._set_string_field(tag, 'dashboard_title', dashboard.title)
 
-        super()._set_string_field(tag, 'look_id', element.look_id)
+        self._set_string_field(tag, 'look_id', element.look_id)
 
         if element.look:
-            super()._set_string_field(tag, 'look_title', element.look.title)
+            self._set_string_field(tag, 'look_title', element.look.title)
 
-        super()._set_string_field(tag, 'lookml_link_id',
-                                  element.lookml_link_id)
+        self._set_string_field(tag, 'lookml_link_id', element.lookml_link_id)
 
         if element.query_id:
-            super()._set_double_field(tag, 'query_id', element.query_id)
+            self._set_double_field(tag, 'query_id', element.query_id)
         elif element.result_maker:
-            super()._set_double_field(tag, 'query_id',
-                                      element.result_maker.query_id)
+            self._set_double_field(tag, 'query_id',
+                                   element.result_maker.query_id)
 
-        super()._set_string_field(tag, 'instance_url', self.__instance_url)
+        self._set_string_field(tag, 'instance_url', self.__instance_url)
 
         return tag
 
@@ -89,27 +87,27 @@ class DataCatalogTagFactory(prepare.BaseTagFactory):
 
         tag.template = tag_template.name
 
-        super()._set_string_field(tag, 'id', folder.id)
-        super()._set_string_field(tag, 'name', folder.name)
-        super()._set_string_field(tag, 'parent_id', folder.parent_id)
-        super()._set_bool_field(tag, 'has_children', folder.child_count > 0)
-        super()._set_double_field(tag, 'children_count', folder.child_count)
+        self._set_string_field(tag, 'id', folder.id)
+        self._set_string_field(tag, 'name', folder.name)
+        self._set_string_field(tag, 'parent_id', folder.parent_id)
+        self._set_bool_field(tag, 'has_children', folder.child_count > 0)
+        self._set_double_field(tag, 'children_count', folder.child_count)
 
         has_dashboards = hasattr(folder, 'dashboards')
         if has_dashboards:
             has_dashboards = True if folder.dashboards else False
-        super()._set_bool_field(tag, 'has_dashboards', has_dashboards)
+        self._set_bool_field(tag, 'has_dashboards', has_dashboards)
         dashboards_count = len(folder.dashboards) if has_dashboards else 0
-        super()._set_double_field(tag, 'dashboards_count', dashboards_count)
+        self._set_double_field(tag, 'dashboards_count', dashboards_count)
 
         has_looks = hasattr(folder, 'looks')
         if has_looks:
             has_looks = True if folder.looks else False
-        super()._set_bool_field(tag, 'has_looks', has_looks)
+        self._set_bool_field(tag, 'has_looks', has_looks)
         looks_count = len(folder.looks) if has_looks else 0
-        super()._set_double_field(tag, 'looks_count', looks_count)
+        self._set_double_field(tag, 'looks_count', looks_count)
 
-        super()._set_string_field(tag, 'instance_url', self.__instance_url)
+        self._set_string_field(tag, 'instance_url', self.__instance_url)
 
         return tag
 
@@ -118,36 +116,35 @@ class DataCatalogTagFactory(prepare.BaseTagFactory):
 
         tag.template = tag_template.name
 
-        super()._set_double_field(tag, 'id', look.id)
-        super()._set_string_field(tag, 'description', look.description)
-        super()._set_string_field(tag, 'folder_id', look.space.id)
-        super()._set_string_field(tag, 'folder_name', look.space.name)
-        super()._set_bool_field(tag, 'is_public', look.public)
-        super()._set_double_field(tag, 'user_id', look.user_id)
-        super()._set_double_field(tag, 'last_updater_id', look.last_updater_id)
-        super()._set_double_field(tag, 'query_id', look.query_id)
+        self._set_double_field(tag, 'id', look.id)
+        self._set_string_field(tag, 'description', look.description)
+        self._set_string_field(tag, 'folder_id', look.space.id)
+        self._set_string_field(tag, 'folder_name', look.space.name)
+        self._set_bool_field(tag, 'is_public', look.public)
+        self._set_double_field(tag, 'user_id', look.user_id)
+        self._set_double_field(tag, 'last_updater_id', look.last_updater_id)
+        self._set_double_field(tag, 'query_id', look.query_id)
 
         # Some objects, such as LookWithDashboards instances, don't have
         # the url attribute.
         if hasattr(look, 'url'):
-            super()._set_string_field(tag, 'url', look.url)
+            self._set_string_field(tag, 'url', look.url)
 
-        super()._set_string_field(tag, 'short_url', look.short_url)
-        super()._set_string_field(tag, 'public_url', look.public_url)
-        super()._set_string_field(tag, 'excel_file_url', look.excel_file_url)
-        super()._set_string_field(tag, 'google_spreadsheet_formula',
-                                  look.google_spreadsheet_formula)
-        super()._set_double_field(tag, 'view_count', look.view_count)
-        super()._set_double_field(tag, 'favorite_count', look.favorite_count)
-        super()._set_timestamp_field(tag, 'last_accessed_at',
-                                     look.last_accessed_at)
-        super()._set_timestamp_field(tag, 'last_viewed_at',
-                                     look.last_viewed_at)
-        super()._set_bool_field(tag, 'is_deleted', look.deleted)
-        super()._set_timestamp_field(tag, 'deleted_at', look.deleted_at)
-        super()._set_double_field(tag, 'deleter_id', look.deleter_id)
+        self._set_string_field(tag, 'short_url', look.short_url)
+        self._set_string_field(tag, 'public_url', look.public_url)
+        self._set_string_field(tag, 'excel_file_url', look.excel_file_url)
+        self._set_string_field(tag, 'google_spreadsheet_formula',
+                               look.google_spreadsheet_formula)
+        self._set_double_field(tag, 'view_count', look.view_count)
+        self._set_double_field(tag, 'favorite_count', look.favorite_count)
+        self._set_timestamp_field(tag, 'last_accessed_at',
+                                  look.last_accessed_at)
+        self._set_timestamp_field(tag, 'last_viewed_at', look.last_viewed_at)
+        self._set_bool_field(tag, 'is_deleted', look.deleted)
+        self._set_timestamp_field(tag, 'deleted_at', look.deleted_at)
+        self._set_double_field(tag, 'deleter_id', look.deleter_id)
 
-        super()._set_string_field(tag, 'instance_url', self.__instance_url)
+        self._set_string_field(tag, 'instance_url', self.__instance_url)
 
         return tag
 
@@ -161,42 +158,42 @@ class DataCatalogTagFactory(prepare.BaseTagFactory):
         model_explore = assembled_query_metadata.model_explore
         connection = assembled_query_metadata.connection
 
-        super()._set_double_field(tag, 'id', query.id)
+        self._set_double_field(tag, 'id', query.id)
 
         if query.fields:
             value = self.__STRING_VALUE_ARRAY_ELEM_SEP.join(query.fields)
-            super()._set_string_field(tag, 'fields', value)
+            self._set_string_field(tag, 'fields', value)
 
         if query.pivots:
             value = self.__STRING_VALUE_ARRAY_ELEM_SEP.join(query.pivots)
-            super()._set_string_field(tag, 'pivots', value)
+            self._set_string_field(tag, 'pivots', value)
 
         if query.sorts:
             value = self.__STRING_VALUE_ARRAY_ELEM_SEP.join(query.sorts)
-            super()._set_string_field(tag, 'sorts', value)
+            self._set_string_field(tag, 'sorts', value)
 
-        super()._set_double_field(tag, 'runtime', query.runtime)
-        super()._set_string_field(tag, 'client_id', query.client_id)
-        super()._set_string_field(tag, 'query_timezone', query.query_timezone)
-        super()._set_string_field(tag, 'lookml_model', query.model)
-        super()._set_string_field(tag, 'explore_name', query.view)
+        self._set_double_field(tag, 'runtime', query.runtime)
+        self._set_string_field(tag, 'client_id', query.client_id)
+        self._set_string_field(tag, 'query_timezone', query.query_timezone)
+        self._set_string_field(tag, 'lookml_model', query.model)
+        self._set_string_field(tag, 'explore_name', query.view)
 
-        super()._set_string_field(tag, 'sql', generated_sql)
+        self._set_string_field(tag, 'sql', generated_sql)
 
         if model_explore:
-            super()._set_string_field(tag, 'lookml_project',
-                                      model_explore.project_name)
-            super()._set_string_field(tag, 'connection',
-                                      model_explore.connection_name)
+            self._set_string_field(tag, 'lookml_project',
+                                   model_explore.project_name)
+            self._set_string_field(tag, 'connection',
+                                   model_explore.connection_name)
 
         if connection:
-            super()._set_string_field(tag, 'host', connection.host)
-            super()._set_string_field(tag, 'database', connection.database)
-            super()._set_string_field(tag, 'connection_dialect',
-                                      connection.dialect_name)
-            super()._set_string_field(tag, 'connection_username',
-                                      connection.username)
+            self._set_string_field(tag, 'host', connection.host)
+            self._set_string_field(tag, 'database', connection.database)
+            self._set_string_field(tag, 'connection_dialect',
+                                   connection.dialect_name)
+            self._set_string_field(tag, 'connection_username',
+                                   connection.username)
 
-        super()._set_string_field(tag, 'instance_url', self.__instance_url)
+        self._set_string_field(tag, 'instance_url', self.__instance_url)
 
         return tag
