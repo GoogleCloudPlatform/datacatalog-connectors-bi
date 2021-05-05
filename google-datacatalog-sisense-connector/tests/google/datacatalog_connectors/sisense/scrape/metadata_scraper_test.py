d8d3cc27ec9ccf19dadbdf5a67e42cab8a8f96c7
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-05-05 11:19:08 -0300

    Add unit tests for the MetadataScraper

--- google-datacatalog-sisense-connector/tests/google/datacatalog_connectors/sisense/scrape/metadata_scraper_test.py
+++ google-datacatalog-sisense-connector/tests/google/datacatalog_connectors/sisense/scrape/metadata_scraper_test.py
@@ -13,3 +13,40 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import unittest
+from unittest import mock
+
+from google.datacatalog_connectors.sisense import scrape
+
+
+class MetadataScraperTest(unittest.TestCase):
+    __SCRAPE_PACKAGE = 'google.datacatalog_connectors.sisense.scrape'
+    __SCRAPER_MODULE = f'{__SCRAPE_PACKAGE}.metadata_scraper'
+
+    @mock.patch(f'{__SCRAPER_MODULE}.rest_api_helper.RESTAPIHelper')
+    def setUp(self, mock_rest_api_helper):
+        self.__scraper = scrape.MetadataScraper(server_address='test-server',
+                                                api_version='test-version',
+                                                username='test-username',
+                                                password='test-password')
+
+    def test_constructor_should_set_instance_attributes(self):
+        attrs = self.__scraper.__dict__
+        self.assertIsNotNone(attrs['_MetadataScraper__api_helper'])
+
+    def test_scrape_all_folders_should_delegate_to_api_helper(self):
+        attrs = self.__scraper.__dict__
+        api_helper = attrs['_MetadataScraper__api_helper']
+
+        folders_metadata = [{
+            '_id': 'folder-id',
+        }]
+
+        api_helper.get_all_folders.return_value = folders_metadata
+
+        folders = self.__scraper.scrape_all_folders()
+
+        self.assertEqual(1, len(folders))
+        self.assertEqual('folder-id', folders[0]['_id'])
+        api_helper.get_all_folders.assert_called_once()
