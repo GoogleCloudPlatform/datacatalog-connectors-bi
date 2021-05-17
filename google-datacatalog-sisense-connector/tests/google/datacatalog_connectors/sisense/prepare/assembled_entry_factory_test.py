53c68b6ad640dbe14b2df504165bc2d6371bcc12
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-05-17 10:39:15 -0300

    Standardize the mock_ prefix usage

--- google-datacatalog-sisense-connector/tests/google/datacatalog_connectors/sisense/prepare/assembled_entry_factory_test.py
+++ google-datacatalog-sisense-connector/tests/google/datacatalog_connectors/sisense/prepare/assembled_entry_factory_test.py
@@ -41,14 +41,14 @@ class AssembledEntryFactoryTest(unittest.TestCase):
             user_specified_system='test-system',
             server_address='https://test.server.com')
 
-        self.__entry_factory = mock_entry_factory.return_value
-        self.__tag_factory = mock_tag_factory.return_value
+        self.__mock_entry_factory = mock_entry_factory.return_value
+        self.__mock_tag_factory = mock_tag_factory.return_value
 
     def test_constructor_should_set_instance_attributes(self):
         attrs = self.__factory.__dict__
 
         self.assertEqual(
-            self.__entry_factory,
+            self.__mock_entry_factory,
             attrs['_AssembledEntryFactory__datacatalog_entry_factory'])
 
     @mock.patch(f'{__PRIVATE_METHOD_PREFIX}'
@@ -114,12 +114,12 @@ class AssembledEntryFactoryTest(unittest.TestCase):
         tag_template.name = 'tagTemplates/sisense_folder_metadata'
 
         fake_entry = ('test-folder', {})
-        entry_factory = self.__entry_factory
+        entry_factory = self.__mock_entry_factory
         entry_factory.make_entry_for_folder.return_value = fake_entry
 
         fake_tag = datacatalog.Tag()
         fake_tag.template = 'tagTemplates/sisense_folder_metadata'
-        tag_factory = self.__tag_factory
+        tag_factory = self.__mock_tag_factory
         tag_factory.make_tag_for_folder.return_value = fake_tag
 
         assembled_entry = self.__factory\
