0407c1709e2294f4c138f629f42dead850bcb09c
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-10-24 11:59:31 -0300

    Split some printer's unit tests

--- google-datacatalog-sisense-connector/tests/google/datacatalog_connectors/sisense/addons/elasticube_dependency_printer_test.py
+++ google-datacatalog-sisense-connector/tests/google/datacatalog_connectors/sisense/addons/elasticube_dependency_printer_test.py
@@ -36,8 +36,10 @@ class ElastiCubeDependencyPrinterTest(unittest.TestCase):
     def test_print_dependency_finder_results_should_skip_if_no_results(
             self, mock_get_asset_metadata_value, mock_filter_jaql_tags):
 
+        # when
         addons.ElastiCubeDependencyPrinter.print_dependency_finder_results({})
 
+        # then
         mock_get_asset_metadata_value.assert_not_called()
         mock_filter_jaql_tags.assert_not_called()
 
@@ -47,11 +49,14 @@ class ElastiCubeDependencyPrinterTest(unittest.TestCase):
     def test_print_dependency_finder_results_should_get_asset_metadata_values(
             self, mock_get_asset_metadata_value):
 
+        # given
         fake_find_results = {'entries/test-entry': (datacatalog.Entry(), [])}
 
+        # when
         addons.ElastiCubeDependencyPrinter.print_dependency_finder_results(
             fake_find_results)
 
+        # then
         mock_get_asset_metadata_value.assert_has_calls(
             [mock.call([], 'dashboard_title'),
              mock.call([], 'datasource')])
@@ -63,38 +68,60 @@ class ElastiCubeDependencyPrinterTest(unittest.TestCase):
     def test_print_dependency_finder_results_should_filter_jaql_tags(
             self, mock_filter_jaql_tags):
 
-        fake_tag = mocks.make_fake_tag(
-            string_fields=[('table', 'test-table'), ('column', 'test-column')])
-
-        # Just to improve code coverage. The Below return value does not impact
-        # test results but causes the script to print Tag's data.
-        mock_filter_jaql_tags.return_value = [fake_tag]
-
+        # given
+        fake_tag = mocks.make_fake_tag()
         fake_find_results = {
             'entries/test-entry': (datacatalog.Entry(), [fake_tag])
         }
 
+        # when
         addons.ElastiCubeDependencyPrinter.print_dependency_finder_results(
             fake_find_results)
 
+        # then
         mock_filter_jaql_tags.assert_called_once_with([fake_tag])
+
+    @mock.patch(f'{__PRINTER_MODULE}.tabulate.tabulate')
+    @mock.patch(f'{__PRINTER_MODULE}.base_finder'
+                f'.ElastiCubeDependencyFinder.filter_jaql_tags')
+    @mock.patch(f'{__PRIVATE_METHOD_PREFIX}__get_asset_metadata_value',
+                lambda *args: None)
+    def test_print_dependency_finder_results_should_tabulate_tags_data(
+            self, mock_filter_jaql_tags, mock_tabulate):
+
+        # given
+        fake_tag = mocks.make_fake_tag(
+            string_fields=[('table', 'test-table'), ('column', 'test-column')])
+        mock_filter_jaql_tags.return_value = [fake_tag]
+        fake_find_results = {
+            'entries/test-entry': (datacatalog.Entry(), [fake_tag])
+        }
+
+        # when
+        addons.ElastiCubeDependencyPrinter.print_dependency_finder_results(
+            fake_find_results)
+
+        # then
+        mock_filter_jaql_tags.assert_called_once_with([fake_tag])
+        mock_tabulate.assert_called_once()
 
     @mock.patch(f'{__PRINTER_MODULE}.base_finder'
                 f'.ElastiCubeDependencyFinder.filter_asset_metadata_tag')
     def test_get_asset_metadata_value_should_return_value_if_available(
             self, mock_filter_asset_metadata_tag):
 
+        # given
         fake_tag = mocks.make_fake_tag(string_fields=[('datasource',
                                                        'test-datasource')])
-
         mock_filter_asset_metadata_tag.return_value = fake_tag
 
+        # when
         value = addons.ElastiCubeDependencyPrinter\
             ._ElastiCubeDependencyPrinter__get_asset_metadata_value(
                 [fake_tag], 'datasource')
 
+        # then
         self.assertEqual('test-datasource', value)
-
         mock_filter_asset_metadata_tag.assert_called_once_with([fake_tag])
 
     @mock.patch(f'{__PRINTER_MODULE}.base_finder'
@@ -102,14 +129,15 @@ class ElastiCubeDependencyPrinterTest(unittest.TestCase):
     def test_get_asset_metadata_value_should_return_none_if_not_available(
             self, mock_filter_asset_metadata_tag):
 
+        # given
         fake_tag = mocks.make_fake_tag(string_fields=[('field', 'test-field')])
-
         mock_filter_asset_metadata_tag.return_value = fake_tag
 
+        # when
         value = addons.ElastiCubeDependencyPrinter\
             ._ElastiCubeDependencyPrinter__get_asset_metadata_value(
                 [fake_tag], 'datasource')
 
+        # then
         self.assertIsNone(value)
-
         mock_filter_asset_metadata_tag.assert_called_once_with([fake_tag])
